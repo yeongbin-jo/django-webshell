62425e61ace776b8b44363a7215419149ea2d3f7
CHIDA <44139236+yeongbin-jo@users.noreply.github.com>
2021-12-14 16:50:31 +0900

    Update urls.py

--- webshell/urls.py
+++ webshell/urls.py
@@ -1,6 +1,6 @@
-from django.conf.urls import url
+from django.urls import re_path
 from .views import execute_script_view
 
 urlpatterns = [
-    url(r'^execute/$', execute_script_view, name='execute-script'),
+    re_path(r'^execute/$', execute_script_view, name='execute-script'),
 ]
